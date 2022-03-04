0191f76109d0c1902464dcb81273f1823447da69
lux-02 <oys128950@gmail.com>
2022-03-04 11:21:28 +0000

    0304

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,94 +2,81 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// parseFloat=> 문자열->실수 변환
-
-function BoilingVerdict(props) {
-	if(props.celsius >= 100) {
-		return <p>The water would boil.</p>;
-	}
-	return <p>The water would not boil.</p>;
+function FancyBorder(props) {
+	return <div className={'FancyBorder FancyBorder-' + props.color}>{props.children}</div>; //prop.children으로 내용 표시
 }
 
-class Calculator extends React.Component {	
-	constructor(props) {
-		super(props);
-		this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
-		this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
-		this.state = {temperature: '', scale: 'c'}
-	}
-	
-	handleCelsiusChange(temperature) {
-		this.setState({scale: 'c', temperature});
-	}
-	
-	handleFahrenheitChange(temperature) {
-		this.setState({scale: 'f', temperature});
-	}
-	
-	render(){
-		const scale = this.state.scale;
-		const temperature = this.state.temperature;
-		const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
-		const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
-		
-		return (
-			<div>
-				<TemperatureInput scale="c" temperature={celsius} onTemperatureChange={this.handleCelsiusChange} />
-				<TemperatureInput scale="f" temperature={fahrenheit} onTemperatureChange={this.handleFahrenheitChange} />
-				<BoilingVerdict celsius={parseFloat(celsius)} />
-			</div>
-		);
-	}
+function Dialog(props){
+	return(
+		<FancyBorder color="blue">
+			<h1 className="Dialog-title">
+				{props.title}
+			</h1>
+			<p className="Dialog-message">
+				{props.message}
+			</p>
+			{props.children}
+		</FancyBorder>
+	);
 }
 
-const scaleNames = {
-	c: 'Celsius',
-	f: 'Fahrenheit'
-}
-
-class TemperatureInput extends React.Component {
+class SignUpDialog extends React.Component {
 	constructor(props) {
 		super(props);
 		this.handleChange = this.handleChange.bind(this);
-		this.state = {temperature: ''};
+		this.handleSignUp = this.handleSignUp.bind(this);
+		this.state = {login: ' '};
+	}
+	
+	render(){
+		return(
+			<Dialog title="Mars Exploration Program" message="How should we refer to you?">
+				<input type="text" value={this.state.login} onChage={this.handleChange} />
+				<button onClick={this.handleSignUp}>Sign Me Up!</button>
+			</Dialog>
+		);
 	}
 	
 	handleChange(e) {
-		//this.setState({temperature: e.target.value});
-		this.props.onTemperatureChange(e.target.value);
+		this.setState({login: e.target.value});
 	}
 	
-	render() { 
-		//const temperature = this.state.temperature;
-		const temperature = this.props.temperature;
-		const scale = this.props.scale;
-		return (
-			<fieldset>
-				<legend>Enter temperature in {scaleNames[scale]}:</legend>
-				<input value={temperature}
-					onChange={this.handleChange} />
-			</fieldset>
-		);
+	handleSignUp(){
+		alert(`Welcome aboard, ${this.state.login}!`);
 	}
 }
 
-function toCelsius(fahrenheit) {
-	return (fahrenheit - 32) * 5 / 9;
+function WelcomeDialog() {
+	return (
+		<Dialog title="Welcome" message="Thank you for visiting our spacecraft" />
+	);
 }
 
-function toFahrenheit(celsius){
-	return (celsius * 9 / 5) + 32;
+function Contacts(){
+	return <div className="Contacts" />;
 }
 
-function tryConvert(temperature, convert) {
-	const input = parseFloat(temperature);
-	if(Number.isNaN(input)) {
-		return '';
-	}
-	const output = convert(input);
-	const rounded = Math.round(output * 1000) / 1000;
-	return rounded.toString();
+function Chat(){
+	return <div className="Chat" />;
 }
 
-ReactDOM.render(<Calculator />, document.getElementById('root'));
+function SplitPane(props){
+	return (
+		<div className="splitPane">
+			<div className="splitPane-left">
+				{props.left}
+			</div>
+			<div className="SplitPane-right">
+				{props.right}
+			</div>
+		</div>
+	);
+}
+
+function App(){
+	return(
+		<SplitPane left={<Contacts />} right={<Chat />}/>
+	);
+}
+
+ReactDOM.render(<SignUpDialog />, document.getElementById('root'));
