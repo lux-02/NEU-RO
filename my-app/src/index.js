f25f49e7d94fd6bd86357ff40e6b5950dfed8011
lux-02 <oys128950@gmail.com>
2022-03-09 04:03:55 +0000

    0309 pre

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -31,7 +31,9 @@ class ContentsArticle extends React.Component {
 		return (
 			<div className="contents_article flex">
 				<img src={this.props.img} alt={this.props.alt} />
-				<div class="contents_section flex"><p>{this.props.con}</p></div>
+				<div className="contents_section flex">
+					<p>{this.props.con}</p>
+				</div>
 			</div>
 		);
 	}
@@ -60,7 +62,7 @@ class Plus extends React.Component {
 
 	render() {
 		return (
-			<div className="plus_contents">
+			<div className="plus_contents flex">
 				<button onClick={this.handleContentsInput}>Update</button>
 			</div>
 		);
@@ -87,11 +89,38 @@ class Logo extends React.Component {
 	}
 }
 
+class ContentsFilterCategory extends React.Component {
+	render() {
+		return (
+			<div className="contents_filterCategory flex">
+				{this.props.category}
+				
+			</div>
+		);
+	}
+}
+
+
+
+class ContentsFilterArea extends React.Component {
+	render() {
+		const clist = this.props.list;
+		const clist_items = clist.map((category) =>(
+			<ContentsFilterCategory key={category} category={category}/>
+		));
+		return (
+			<div className="contents_filter_area flex">
+				{clist_items}
+			</div>
+		);
+	}
+}
+
 class ContentsCnt extends React.Component {
 	render() {
 		return (
 			<div className="contents_cnt flex">
-				<p>지금 {this.props.cnt}개의 뉴스레터를 만나보세요.</p>
+				<p>당신을 위한 {this.props.cnt}개의 뉴스레터를 만나보세요.</p>
 			</div>
 		);
 	}
@@ -102,7 +131,7 @@ class Contents extends React.Component {
 		const clist = this.props.list;
 		const clist_items = clist.map((contents) => (
 			<div className="clist">
-				<a className="clist_alink" href={contents.url}>
+				<a className="clist_alink" target="_blank" href={contents.url}>
 					<ContentsItem
 						img={contents.img}
 						contents={contents.contents}
@@ -111,7 +140,6 @@ class Contents extends React.Component {
 						field={contents.field}
 						con={contents.contents}
 						upload={contents.upload}
-						
 					/>
 				</a>
 			</div>
@@ -140,17 +168,23 @@ class Wrap extends React.Component {
 			<div className="wrap flex">
 				<Header />
 				<ContentsCnt cnt={contents_list.length} />
+				<ContentsFilterArea list={category_list} />
 				<Contents list={contents_list} />
 			</div>
 		);
 	}
 }
 
+const non_img =
+	'https://images.unsplash.com/photo-1594322436404-5a0526db4d13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1129&q=80';
+
+const category_list = ['IT', 'Trends', 'UI/UX', 'Culture'];
+
 const contents_list = [
 	{
 		field: 'IT',
 		title: 'ZDNet Korea',
-		img: 'https://bit.ly/3KqqJAl',
+		img: 'https://zdnet.co.kr/images/footer_logo.png',
 		upload: '매일',
 		url: 'http://www.zdnet.co.kr/',
 		contents: '국내 IT전문 언론사 중 가장 폭 넒은 범위의 기사를 제공',
@@ -158,7 +192,7 @@ const contents_list = [
 	{
 		field: 'IT',
 		title: 'ITWorld Korea',
-		img: 'https://bit.ly/3KqqJAl',
+		img: 'https://www.itworld.co.kr/_images/common/logo-itw.svg',
 		upload: '매일',
 		url: 'http://www.itworld.co.kr',
 		contents:
@@ -167,7 +201,7 @@ const contents_list = [
 	{
 		field: 'IT',
 		title: 'CIO Korea',
-		img: 'https://bit.ly/3KqqJAl',
+		img: 'https://www.ciokorea.com/_images/common/logo-cio.svg',
 		upload: '매일',
 		url: 'http://www.ciokorea.com',
 		contents:
@@ -176,7 +210,7 @@ const contents_list = [
 	{
 		field: 'IT',
 		title: 'VIEWS LETTER',
-		img: 'https://bit.ly/3KqqJAl',
+		img: 'https://bit.ly/3sRWdt7',
 		upload: '매일',
 		url: 'https://themiilk.com/',
 		contents: '미국 실리콘밸리 현장의 소식 및 인사이트 등	',
@@ -184,7 +218,8 @@ const contents_list = [
 	{
 		field: 'IT',
 		title: '블로터',
-		img: 'https://bit.ly/3KqqJAl',
+		img:
+			'https://scontent-ssn1-1.xx.fbcdn.net/v/t1.6435-9/118424228_3807028145980697_7355663210502523866_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=e3f864&_nc_ohc=xwuiWDiDa-oAX8fZflz&_nc_ht=scontent-ssn1-1.xx&oh=00_AT-2H6sam28FTP6hecnnWY_ob0VhGG6p06vjP144SSpzSw&oe=624E69FA',
 		upload: '매일',
 		url: 'http://www.bloter.net/',
 		contents:
@@ -193,7 +228,8 @@ const contents_list = [
 	{
 		field: 'IT',
 		title: '바이라인네트워크',
-		img: 'https://bit.ly/3KqqJAl',
+		img:
+			'https://yt3.ggpht.com/WArkdlAhJ-WrRsiBfs8n3evaNKhOnR8ioRaQ0aElm5SekROCPi-teEtodf4a6E1_mAHKCqeioQ=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj',
 		upload: '매일',
 		url: 'https://byline.network/',
 		contents:
@@ -202,7 +238,7 @@ const contents_list = [
 	{
 		field: 'IT',
 		title: 'TECHWORLD',
-		img: 'https://bit.ly/3KqqJAl',
+		img: 'https://cdn.epnc.co.kr/image/logo/watermark_20210111114848.png',
 		upload: '매월',
 		url: 'http://www.epnc.co.kr/',
 		contents: '소재, 솔루션 등에 대한 인더스트리 관련 정보가 많음',
@@ -210,7 +246,8 @@ const contents_list = [
 	{
 		field: 'IT',
 		title: 'techNeedle',
-		img: 'https://bit.ly/3KqqJAl',
+		img:
+			'https://scontent-ssn1-1.xx.fbcdn.net/v/t1.6435-9/31947193_1517493948357146_5727339271468089344_n.png?_nc_cat=105&ccb=1-5&_nc_sid=e3f864&_nc_ohc=UFsNfIadw0oAX8g9Vz-&_nc_ht=scontent-ssn1-1.xx&oh=00_AT-D7u0WYA-5lWaEPhd8ShgphP_bYgMzYY2sFjvNYPI7Fw&oe=624D3AB9',
 		upload: '수시',
 		url: 'http://techneedle.com/',
 		contents: '주로 실리콘밸리에서 근무 또는 거주하는 IT업 종사자의 칼럼 형식 기사를 제공',
@@ -218,23 +255,23 @@ const contents_list = [
 	{
 		field: 'IT',
 		title: '모비인사이드',
-		img: 'https://bit.ly/3KqqJAl',
+		img: non_img,
 		upload: '매월',
 		url: 'https://www.mobiinside.com/kr/',
 		contents: '주로 스타트업, 벤처캐피탈 전반의 소식을 제공',
 	},
 	{
-		field: 'IT',
+		field: 'IT1',
 		title: '플래텀',
-		img: 'https://bit.ly/3KqqJAl',
+		img: 'https://platum.kr/wp-content/uploads/2016/11/logo-2.png',
 		upload: '매월',
 		url: 'https://platum.kr/',
 		contents: '스타트업 소식 전문 언론',
 	},
 	{
-		field: 'IT',
+		field: 'IT2',
 		title: 'Startup Weekly',
-		img: 'https://bit.ly/3KqqJAl',
+		img: non_img,
 		upload: '매월',
 		url: 'http://www.startupweekly.net/news',
 		contents:
